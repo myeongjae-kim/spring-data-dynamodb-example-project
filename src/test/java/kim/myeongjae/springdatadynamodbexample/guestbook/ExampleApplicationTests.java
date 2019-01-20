9554ceacade0a7107715005b37401bcec9b20485
김명재 myeongjae.kim <dev@myeongjae.kim>
2019-01-20 16:16:56 +0900

    [master] First Commit
    - Initialize a project by Spring Boot Intializer.

---
+++ src/test/java/kim/myeongjae/springdatadynamodbexample/guestbook/ExampleApplicationTests.java
@@ -0,0 +1,17 @@
+package kim.myeongjae.springdatadynamodbexample.guestbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
